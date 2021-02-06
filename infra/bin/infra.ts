cb700cae22d3f37a8717e3590715ac9be696669f
kaedenwile <kaeden.wile@outlook.com>
2021-02-06 12:29:32 -0800

    Add cloudfront distribution to infra

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,6 +3,6 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { InfraStack } from '../lib/infra-stack';
 
-const env = {region: 'us-west-1'};
+const env = {region: 'us-west-2'};
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {env});
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,24 +1,38 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+
+import * as acm from '@aws-cdk/aws-certificatemanager';
+
 import * as route53 from '@aws-cdk/aws-route53';
-import { BucketWebsiteTarget } from '@aws-cdk/aws-route53-targets';
+import * as targets from '@aws-cdk/aws-route53-targets';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    let websiteBucket = new s3.Bucket(this, 'wile.xyz', {
+    let websiteBucket = new s3.Bucket(this, 'websiteBucket', {
       websiteIndexDocument: 'index.html'
     });
 
-    let zone = new route53.PublicHostedZone(this, 'HostedZone', {
+    let websiteHostedZone = new route53.PublicHostedZone(this, 'websiteHostedZone', {
       zoneName: 'wile.xyz'
     });
 
+    let websiteCert = acm.Certificate.fromCertificateArn(this, 'websiteCert','arn:aws:acm:us-east-1:745658675557:certificate/ff7ac770-3f08-4896-8831-38495304a758');
+
+    let websiteDistribution = new cloudfront.Distribution(this, 'websiteDistribution', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+      domainNames: ['wile.xyz'],
+      certificate: websiteCert
+    });
+
     new route53.ARecord(this, 'ARecord', {
-      zone,
-      target: route53.RecordTarget.fromAlias(new BucketWebsiteTarget(websiteBucket)),
-    })
+      zone: websiteHostedZone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(websiteDistribution)),
+    });
 
     // Note that NS and SOA records are created automatically
     // Remember to manually update name servers on the domain
