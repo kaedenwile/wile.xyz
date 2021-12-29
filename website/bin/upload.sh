80a3e2df80a2fa1e83bb7ba1176853622a981151
Kaeden Wile <kaeden.wile@gmail.com>
2021-12-29 12:11:51 -0800

    Deploy to prod bucket only on push to main

--- website/bin/upload.sh
+++ website/bin/upload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 STACK_NAME=InfraStack
+BUCKET_NAME=${BUCKET_NAME:-devBucket} # Use `websiteBucket` instead of `devBucket` for prod
 
 # shellcheck disable=SC2016
 BUCKET_NAME=$(
   aws cloudformation describe-stack-resources --stack-name "$STACK_NAME" \
-  --query 'StackResources[?starts_with(LogicalResourceId, `devBucket`)].PhysicalResourceId' \
+  --query "StackResources[?starts_with(LogicalResourceId, \`$BUCKET_NAME\`)].PhysicalResourceId" \
   --output text
 )
-# Use `devBucket` instead of `websiteBucket` for testing
 
 [ -z "$BUCKET_NAME" ] && exit 1
 
@@ -19,4 +19,4 @@ for html in $(find dist -name '*.html'); do
   aws s3 cp $html "s3://$BUCKET_NAME/$(echo $html | sed "s/dist\/\(.*\)\.html$/\1/")" --content-type text/html --acl bucket-owner-full-control --acl public-read --metadata-directive REPLACE
 done
 
-echo "\n******************\n* UPLOADED TO:\n*\thttp://${BUCKET_NAME}.s3-website-us-west-2.amazonaws.com\n******************"
+echo -e "\n******************\n* UPLOADED TO:\n*\thttp://${BUCKET_NAME}.s3-website-us-west-2.amazonaws.com\n******************"
