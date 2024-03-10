bcd499ae21a2df7280bbb387559de6355240442a
Kaeden Wile <kaeden.wile@gmail.com>
2024-03-10 14:27:08 -0500

    Set up Vite project

---
+++ pkg/site/src/Router.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Home } from './home/Home.tsx';
+
+export const Router = () => {
+    const [route] = useState<string>(window.location.pathname);
+
+    if (route === '/') {
+        return <Home />;
+    }
+};
