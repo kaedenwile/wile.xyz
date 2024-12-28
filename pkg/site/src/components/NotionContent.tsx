935957b5ae3855401b3a35ddea90d4593b5b40e6
Kaeden Wile <kaeden.wile@gmail.com>
2024-12-28 11:49:32 -0800

    Update page copy

--- pkg/site/src/components/NotionContent.tsx
+++ pkg/site/src/components/NotionContent.tsx
@@ -10,20 +10,19 @@ export type NotionContentProps = {
 export const NotionContent = ({ content }: NotionContentProps) => {
   const children: ReactNode[] = [];
 
-  let i = 0;
   for (const block of content ?? []) {
-    const element = <Block block={block} key={i++} />;
+    const element = <Block block={block} key={block.id} />;
 
     if (block.type === 'bulleted_list_item') {
       if ((children[children.length - 1] as ReactElement)?.type === 'ul') {
         children.push(
-          <ul key={i}>
+          <ul key={block.id + '-ul'}>
             {(children.pop() as ReactElement).props.children}
             {element}
           </ul>
         );
       } else {
-        children.push(<ul key={i}>{element}</ul>);
+        children.push(<ul key={block.id + '-ul'}>{element}</ul>);
       }
     } else {
       children.push(element);
